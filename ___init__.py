59486e577e97c12f479afe971d54772170fb5ddb
will-cotton4 <will.cotton4@gmail.com>
2019-05-29 14:55:27 -0500

    Added football stuff

---
+++ ___init__.py
@@ -0,0 +1,10 @@
+#!C:\Users\willc\Anaconda3 python
+"""
+lambdata - a collection of Data Science helper functions
+"""
+
+import pandas as pd
+import numpy as np
+import example_module
+
+TEST = pd.DataFrame(np.ones(10))
---
+++ example_module.py
@@ -0,0 +1,4 @@
+def increment(number):
+	return number + 1
+x=2
+x = increment(x)
